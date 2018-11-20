fccf3298a65be688c43879f44d6514f7fe03e130
Mike Hurd <strangled1@yahoo.com>
2018-11-19 17:56:20 -0700

    Initial commit.

--- cred01.py
+++ cred01.py
@@ -10,6 +10,7 @@ import os
 
 # Suppress warnings
 import warnings
+
 warnings.filterwarnings('ignore')
 
 # matplotlib and seaborn for plotting
@@ -45,6 +46,7 @@ def missing_values_table(df):
     # Return the dataframe with missing information
     return mis_val_table_ren_columns
 
+
 # Training data
 app_train = pd.read_csv('./data/application_train.csv')
 print('Training data shape: ', app_train.shape)
@@ -57,7 +59,44 @@ print(app_test.head())
 
 print(app_train['TARGET'].value_counts())
 
-app_train['TARGET'].astype(int).plot.hist();
+plt.figure()
+app_train['TARGET'].astype(int).plot.hist(title='Target Distribution');
+plt.xlabel('Target Distribution');
+
+# Create a label encoder object
+le = LabelEncoder()
+le_count = 0
+
+# Iterate through the columns
+for col in app_train:
+    if app_train[col].dtype == 'object':
+        # If 2 or fewer unique categories
+        if len(list(app_train[col].unique())) <= 2:
+            # Train on the training data
+            le.fit(app_train[col])
+            # Transform both training and testing data
+            app_train[col] = le.transform(app_train[col])
+            app_test[col] = le.transform(app_test[col])
+
+            # Keep track of how many columns were label encoded
+            le_count += 1
+
+print('%d columns were label encoded.' % le_count)
+
+# one-hot encoding of categorical variables
+app_train = pd.get_dummies(app_train)
+app_test = pd.get_dummies(app_test)
+
+print('Training Features shape: ', app_train.shape)
+print('Testing Features shape: ', app_test.shape)
+
+train_labels = app_train['TARGET']
+
+# Align the training and testing data, keep only columns present in both dataframes
+app_train, app_test = app_train.align(app_test, join='inner', axis=1)
+
+# Add the target back in
+app_train['TARGET'] = train_labels
 
 # Missing values statistics
 missing_values = missing_values_table(app_train)
@@ -69,7 +108,58 @@ result = app_train.dtypes.value_counts()
 print(result)
 
 # Number of unique classes in each object column
-result = app_train.select_dtypes('object').apply(pd.Series.nunique, axis = 0)
+result = app_train.select_dtypes('object').apply(pd.Series.nunique, axis=0)
 print(result)
+
+# ANOMOLIES
+(app_train['DAYS_BIRTH'] / -365).describe()
+
+plt.figure()
+app_train['DAYS_EMPLOYED'].plot.hist(title='Days Employment Histogram');
+plt.xlabel('Days Employment');
+
+anom = app_train[app_train['DAYS_EMPLOYED'] == 365243]
+non_anom = app_train[app_train['DAYS_EMPLOYED'] != 365243]
+print('The non-anomalies default on %0.2f%% of loans' % (100 * non_anom['TARGET'].mean()))
+print('The anomalies default on %0.2f%% of loans' % (100 * anom['TARGET'].mean()))
+print('There are %d anomalous days of employment' % len(anom))
+
+# Create an anomalous flag column
+app_train['DAYS_EMPLOYED_ANOM'] = app_train["DAYS_EMPLOYED"] == 365243
+
+# Replace the anomalous values with nan in training data
+app_train['DAYS_EMPLOYED'].replace({365243: np.nan}, inplace = True)
+
+# Replace the anomalous values with nan in testing data
+app_test['DAYS_EMPLOYED_ANOM'] = app_test["DAYS_EMPLOYED"] == 365243
+app_test["DAYS_EMPLOYED"].replace({365243: np.nan}, inplace = True)
+
+print('There are %d anomalies in the test data out of %d entries' % (app_test["DAYS_EMPLOYED_ANOM"].sum(), len(app_test)))
+
+plt.figure()
+app_train['DAYS_EMPLOYED'].plot.hist(title = 'Days Employment Histogram');
+plt.xlabel('Days Employment');
+
+# Identify Correlating Features
+# Find correlations with the target and sort
+correlations = app_train.corr()['TARGET'].sort_values()
+
+# Display correlations
+print('Most Positive Correlations:\n', correlations.tail(15))
+print('\nMost Negative Correlations:\n', correlations.head(15))
+
+# Find the correlation of the positive days since birth and target
+app_train['DAYS_BIRTH'] = abs(app_train['DAYS_BIRTH'])
+app_train['DAYS_BIRTH'].corr(app_train['TARGET'])
+
+# Set the style of plots
+plt.figure()
+plt.style.use('fivethirtyeight')
+
+# Plot the distribution of ages in years
+plt.hist(app_train['DAYS_BIRTH'] / 365, edgecolor = 'k', bins = 25)
+plt.title('Age of Client'); plt.xlabel('Age (years)'); plt.ylabel('Count');
+
+
 
 plt.show()
